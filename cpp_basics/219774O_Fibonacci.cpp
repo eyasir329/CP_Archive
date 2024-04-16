1efb8fbdd1ee61a304ac1d7a6cc6036021e1f906
Eyasir Ahamed <eyasir329@gmail.com>
2024-04-17 01:56:42 +0600

    cpp basics p2

---
+++ cpp_basics/219774O_Fibonacci.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define all(a) a.begin(),a.end()
+#define sz(a) ((int) a.size())
+#define pb push_back
+#define pp pop_back
+#define ff first
+#define ss second
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+	int n; cin >> n;
+	int first = 0, second = 1, third;
+	if (n == 1) cout << 0;
+	else if (n == 2) cout << 1;
+	else {
+		while (n > 2) {
+			third = first + second;
+			first = second;
+			second = third;
+			n--;
+		}
+		cout << third;
+	}
+}
+
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+	return 0;
+}
