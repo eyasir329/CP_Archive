16f95b0cbbf9362e9b763c14e5dfc5c3988572be
Eyasir Ahamed <eyasir329@gmail.com>
2025-04-12 06:12:15 +0600

    update

--- docs/generate_readmes.sh
+++ docs/generate_readmes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-for dir in docs/{00..18}_*; do
+set -e  # Exit on error
+
+for dir in {00..18}_*/; do
   cat > "$dir/README.md" <<EOF
 ---
 layout: category
-title: $(basename "$dir" | sed 's/_/ /g')
+title: ${dir//_/ }
 ---
 
-### Problem List
-$(find "$dir" -name "*.cpp" | while read file; do
-  echo "- [$(basename "$file" .cpp)]($(basename "$file"))"
-done)
+### Solutions
+$(find "$dir" -name "*.cpp" | sed 's|.*/|- [&](&)|; s/.cpp//')
 EOF
 done
